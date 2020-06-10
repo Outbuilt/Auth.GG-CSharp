7452d2598a1590d5dfa685b3c4616e5fdf89f512
David Hazy <support@auth.gg>
2020-06-10 15:52:50 -0700

    Fixed SSL Error
    We are now printing the actual error in your code instead of the SSL error, also made it work throughout all drives and not just C:

--- Console Example/Auth.GG Example/API.cs
+++ Console Example/Auth.GG Example/API.cs
@@ -241,9 +241,9 @@ namespace AuthGG
                     }
                     Security.End();
                 }
-                catch
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to establish a secure SSL tunnel with the server!", Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(ex.Message, Name, MessageBoxButton.OK, MessageBoxImage.Error);
                     Process.GetCurrentProcess().Kill();
                 }
             }
@@ -287,9 +287,9 @@ namespace AuthGG
                     }))).Split("|".ToCharArray()));
                     Security.End();
                 }
-                catch
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to establish a secure SSL tunnel with the server!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(ex.Message, OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Error);
                     Process.GetCurrentProcess().Kill();
                 }
             }
@@ -412,9 +412,9 @@ namespace AuthGG
                             return false;
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to establish a secure SSL tunnel with the server!", ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(ex.Message, ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
                     Security.End();
                     Process.GetCurrentProcess().Kill();
                 }
@@ -487,9 +487,9 @@ namespace AuthGG
 
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to establish a ssecure SSL tunnel with the server!", ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(ex.Message, ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
                     Process.GetCurrentProcess().Kill();
                 }
                 return false;
@@ -597,9 +597,9 @@ namespace AuthGG
                             return false;
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to establish a secure SSL tunnel with the server!", ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(ex.Message, ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
                     Security.End();
                     Process.GetCurrentProcess().Kill();
                 }
@@ -684,9 +684,9 @@ namespace AuthGG
                             return false;
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to establish a secure SSL tunnel with the server!", ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(ex.Message, ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
                     Process.GetCurrentProcess().Kill();
                 }
                 return false;
@@ -761,9 +761,9 @@ namespace AuthGG
                             return false;
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Failed to establish a secure SSL tunnel with the server!", ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(ex.Message, ApplicationSettings.Name, MessageBoxButton.OK, MessageBoxImage.Error);
                     Process.GetCurrentProcess().Kill();
                 }
                 return false;
@@ -797,6 +797,7 @@ namespace AuthGG
         }
         public static void Start()
         {
+            string drive = Path.GetPathRoot(Environment.SystemDirectory);
             if (Constants.Started)
             {
                 MessageBox.Show("A session has already been started, please end the previous one!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Warning);
@@ -804,7 +805,7 @@ namespace AuthGG
             }
             else
             {
-                using (StreamReader sr = new StreamReader(@"C:\Windows\System32\drivers\etc\hosts"))
+                using (StreamReader sr = new StreamReader($@"{drive}Windows\System32\drivers\etc\hosts"))
                 {
                     string contents = sr.ReadToEnd();
                     if (contents.Contains("api.auth.gg"))
@@ -1016,8 +1017,9 @@ namespace AuthGG
 
         private string GetArpTable()
         {
+            string drive = Path.GetPathRoot(Environment.SystemDirectory);
             ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = @"C:\Windows\System32\arp.exe";
+            start.FileName = $@"{drive}Windows\System32\arp.exe";
             start.Arguments = "-a";
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
